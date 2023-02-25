23b5a9c6182d97e1e064b9577b23c1c0208812e5
2EN <2381773226@QQ.COM>
2023-02-25 12:23:38 +0800

    初始化项目

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,27 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '屈桓语的小站',
   description: '屈桓语的小站',
-  base: './'
+  base: './',
+  head: [
+    ["link",{rel: "stylesheet",href:"/css/index.css"}]
+  ],
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+    sidebar: [
+      {
+        text: '三岁生日',
+        link: '/post/birthday',
+      },
+    ]
+  }),
 })
